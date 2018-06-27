af102c71b2a5e2d018038add8403c7d6867323de
kreczko <lkreczko@googlemail.com>
2018-06-27 15:08:17 +0100

    adjustments for workernode grid ENV

--- worker_node/00_grid_c7.sh
+++ worker_node/00_grid_c7.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/timeout -s9 10s
-if [ $USER != root ]; then
+
+if [ -z ${USER+x} ]
+then
+	USER=`whoami`
+fi
+
+if [ "$USER" != root ]
+then
 	source /cvmfs/grid.cern.ch/umd-c7wn-latest/etc/profile.d/setup-c7-wn-example.sh
-	# to fix java for the hadoop commands:
-	unset JAVA_HOME
 else
 	echo "User root detected - not loading grid setup"
 fi
